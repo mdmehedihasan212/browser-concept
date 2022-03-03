712e3111c1ea61857b404c2e7cd8fad151617a72
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-04 00:56:48 +0600

    add setTimeOut

--- time-out.js
+++ time-out.js
@@ -1,4 +1,8 @@
+function certificate() {
+    console.log("i am grasuate");
+}
 console.log("first: i am first here");
 console.log("second: i am second here");
 console.log("third: i am third here");
+setTimeout(certificate)
 console.log("final: i am fourth here");
