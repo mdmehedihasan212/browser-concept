a8834eb6843c627b3f6e33d753278d7bb89802e8
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-04 01:06:46 +0600

    explore setTimeOut

--- time-out.js
+++ time-out.js
@@ -1,8 +1,14 @@
 function certificate() {
-    console.log("i am grasuate");
+    console.log("i am Expert");
 }
 console.log("first: i am first here");
 console.log("second: i am second here");
 console.log("third: i am third here");
-setTimeout(certificate)
+setTimeout(certificate, 6000);
+setTimeout(function () {
+    console.log("i am use VS code");
+}, 5000);
+setTimeout(() => {
+    console.log("i am learning web development");
+}, 4000);
 console.log("final: i am fourth here");
