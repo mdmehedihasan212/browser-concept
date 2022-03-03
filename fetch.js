a781cb44a93ad3321d25a62b58fd34ed0310fd74
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-04 02:03:09 +0600

    fetch work

---
+++ fetch.js
@@ -0,0 +1,8 @@
+console.log(4562);
+console.log(8562);
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+console.log(9562);
+setTimeout(() => console.log("I am done"), 5000);
+console.log(5562);
